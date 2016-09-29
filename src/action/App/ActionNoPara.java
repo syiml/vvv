7b2562d51adb5bbc41babe3859e2376b3e464b75
syimlzhu <943986080@qq.com>
2016-09-29 23:51:48 +0800

    app获取本oj最近比赛

--- src/action/App/ActionNoPara.java
+++ src/action/App/ActionNoPara.java
@@ -1,8 +1,12 @@
 package action.App;
 
 import action.BaseAction;
+import entity.Contest;
+import net.sf.json.JSONArray;
 import org.apache.struts2.ServletActionContext;
 import org.jsoup.nodes.Document;
+import servise.ContestMain;
+import util.HTML.contestListHTML;
 import util.JSON.AppJson;
 import util.JSON.JSON;
 import util.Main;
@@ -10,6 +14,7 @@ import util.MyClient;
 import util.Tool;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 /**
  * 没有参数的请求
@@ -24,6 +29,22 @@ public class ActionNoPara extends BaseAction{
     }
     public String getOtherOjsContest(){
         out.print(JSON.getOtherOjsContest());
+        return NONE;
+    }
+    public String getRecentlyContestTable(){
+        List<Contest> list = ContestMain.getContests(0,6,-1,null,-1,-1);
+        JSONArray ja = new JSONArray();
+        for(Contest c:list){
+            ja.add(JSON.getJSONObject(
+                    "cid",c.getCid()+"",
+                    "name",c.getName(),
+                    "begin",c.getBeginDate()+"",
+                    "end",c.getEndTime()+"",
+                    "type",c.getType()+"",
+                    "kind",c.getKind()+""
+                    ));
+        }
+        out.print(ja.toString());
         return NONE;
     }
 }
