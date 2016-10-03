9a5110e13a3d4ae96c7647fc491d12729563bd3a
syimlzhu <943986080@qq.com>
2016-10-03 12:11:13 +0800

    修复bug

--- src/util/MyClient.java
+++ src/util/MyClient.java
@@ -140,7 +140,7 @@ public class MyClient extends DefaultHttpClient{
         try {
             HttpGet httpget = new HttpGet(URL);
             httpget.getParams().setBooleanParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, false);
-            httpget.setHeader("Authorization","JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk0Mzk4NjA4MEBxcS5jb20iLCJleHAiOjE0NzU0MTI3NTEsImVtYWlsIjoiOTQzOTg2MDgwQHFxLmNvbSIsInVzZXJfaWQiOjc5MDB9._BckR4ARfWfGLqFkrfrZtwkdqboNqEUW1jATxpd_Rc4");
+            httpget.setHeader("Authorization","JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijk0Mzk4NjA4MEBxcS5jb20iLCJleHAiOjE0NzYzMjY4OTMsImVtYWlsIjoiOTQzOTg2MDgwQHFxLmNvbSIsInVzZXJfaWQiOjc5MDB9.Ubz4nqF5Jql6IkZI0sH2zV2AyeDFXaL_uaCj0knrHQQ");
             HttpResponse hr = execute(httpget);
             entity = hr.getEntity();
             return Jsoup.parse(entity.getContent(), "utf-8", "");
