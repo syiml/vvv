af5e08bbc64c0f34c2a22470e7518b60784a570f
syimlzhu <943986080@qq.com>
2016-11-02 19:14:20 +0800

    增加本日每日一题奖励记录

--- src/util/TimerTasks/OneProblemEveryDay.java
+++ src/util/TimerTasks/OneProblemEveryDay.java
@@ -7,6 +7,7 @@ import entity.Enmu.AcbOrderType;
 import entity.OJ.CodeVS.CodeVS;
 import entity.Result;
 import entity.Status;
+import org.apache.commons.collections.set.SynchronizedSet;
 import servise.ContestMain;
 import servise.GvMain;
 import servise.MessageMain;
@@ -15,9 +16,7 @@ import util.SQL.SQL;
 import util.Submitter;
 import util.Tool;
 
-import java.util.Calendar;
-import java.util.List;
-import java.util.Timer;
+import java.util.*;
 
 import static util.Main.GV;
 import static util.Main.status;
@@ -28,7 +27,7 @@ import static util.Main.status;
  * Created by QAQ on 2016/9/22.
  */
 public class OneProblemEveryDay extends MyTimer{
-
+    private static final Set<String> awardsUser = Collections.synchronizedSet(new HashSet<String>());
     @Override
     public void run() {
         Calendar ca = Calendar.getInstance();
@@ -49,6 +48,7 @@ public class OneProblemEveryDay extends MyTimer{
             }
             ContestMain.ResetContestProblmes(cid,pidstring);
             GvMain.setOneProblemEveryDayPid(pid);
+            awardsUser.clear();
         }
     }
 
@@ -98,6 +98,12 @@ public class OneProblemEveryDay extends MyTimer{
             int acNum = new SQL("SELECT COUNT(*) FROM statu WHERE pid=? AND ruser=? AND result=? AND id<?",pid,s.getUser(),Result.AC.getValue(),s.getRid()).queryNum();
             if(acNum == 0){
                 //addacb
+                synchronized (awardsUser){
+                    if(awardsUser.contains(s.getUser())){
+                        return ;
+                    }
+                    awardsUser.add(s.getUser());
+                }
                 Main.users.addACB(s.getUser(),100, AcbOrderType.ONE_PROBLEM_EVERYDAY,"pid:"+pid);
                 MessageMain.addMessageOneProblemEveryDay(s.getUser(),pid,100);
             }
@@ -105,7 +111,7 @@ public class OneProblemEveryDay extends MyTimer{
     }
     @Override
     public void getTimer() throws Exception {
-        setEveryDay(19,33,0);
+        setEveryDay(4,0,0);
         new Timer().scheduleAtFixedRate(this, date, period);
     }
 }
