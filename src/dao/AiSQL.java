16041d800a5e1573f466d9e46447b378f46bb6d2
Mysorrows <1343775090@qq.com>
2017-08-11 22:16:26 +0800

    修改AISQL，添加单例模式，继承BaseCacheLRU。

--- src/dao/AiSQL.java
+++ src/dao/AiSQL.java
@@ -3,18 +3,35 @@ package dao;
 import entity.AiInfo;
 import util.SQL.SQL;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created on 2017/8/6.
  */
-public class AiSQL {
+public class AiSQL extends BaseCacheLRU<Integer,AiInfo> {
 
-    public static List<AiInfo> getAiInfoList(String username){
+    private static AiSQL sql = new AiSQL();
+    /**
+     * 饿汉式单例
+     * @return AiSQL对象
+     */
+    public static AiSQL getInstance(){
+        return sql;
+    }
+    /**
+     * 为private不允许直接new，必须通过getInstance()返回单例
+     */
+    private AiSQL() {
+        super(10);
+    }
+
+    public List<AiInfo> getAiInfoList(String username){
         return new SQL("SELECT * FROM t_ai_info WHERE username= ?",username).queryBeanList(AiInfo.class);
     }
 
-    public static String addAiInfo(String username,int game_id,String aiName,String code,String introduce){
+    public String addAiInfo(String username,int game_id,String aiName,String code,String introduce){
         if (new SQL("INSERT INTO t_ai_info VALUES(?,?,?,?,?,?)",0,username,game_id,aiName,code,introduce).update() !=0){
             return "success";
         }
@@ -22,4 +39,19 @@ public class AiSQL {
     }
 
 
+    @Override
+    protected AiInfo getByKeyFromSQL(Integer key) {
+        ResultSet rs= new SQL("SELECT username,game_id,ai_name,ai_code,introduce " +
+                "FROM t_ai_info WHERE id = ? ", key).query();
+        try {
+            if (rs.next()){
+                return new AiInfo(rs.getString(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getString(5));
+            }else{
+                return null;
+            }
+        }catch(SQLException e){
+            e.getErrorCode();
+        }
+        return null;
+    }
 }
